6ad99bf4a8e778732efd45f2706a69d07fc8e0b4
kibi <69326919+kibigowee@users.noreply.github.com>
2023-03-07 22:17:10 -0800

    Update getWeatherData.js

--- getWeatherData.js
+++ getWeatherData.js
@@ -44,7 +44,7 @@ function fn_getWeatherByLL(geoLat,geoLng){
   //https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,weathercode&models=best_match&temperature_unit=fahrenheit
   var URLRequest = dsAPI + "forecast?latitude=" + String(geoLat) + "&longitude=" + String(geoLng) + "&hourly=temperature_2m,weathercode&models=best_match&temperature_unit=fahrenheit";
   //Make the jQuery.getJSON request
-  $.getJSON( URLRequest )
+  XMLHttpRequest.getJSON( URLRequest )
     //Success promise
     .done(function( data ) {
       var wTemperature = data.current_weather.temperature;
