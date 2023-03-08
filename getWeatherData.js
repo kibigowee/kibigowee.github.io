2b8b81bb6715182bd2ae73fa7fee74ae5addec49
kibi <69326919+kibigowee@users.noreply.github.com>
2023-03-07 22:04:47 -0800

    Update getWeatherData.js
    added the api that should connect to Open Meteo, fingers crossed!!

--- getWeatherData.js
+++ getWeatherData.js
@@ -11,6 +11,8 @@ if (navigator.geolocation) {
     //prompt for city?
 }
 
+fn_getWeatherByLL();
+
 function getPosSuccess(pos){
     // get coords and accuracy properties from returned object
     var geoLat = pos.coords.latitude.toFixed(5);
@@ -32,4 +34,27 @@ function getPosErr(err){
         default:
             alert("an unknown error occured");
     }
+}
+
+
+function fn_getWeatherByLL(geoLat,geoLng){
+  //API Variables
+  var dsAPI = "https://api.open-meteo.com/v1/";
+  //Concatenate API Variables into a URLRequest
+  //https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,weathercode&models=best_match&temperature_unit=fahrenheit
+  var URLRequest = dsAPI + "forecast?latitude=" + String(geoLat) + "&longitude=" + String(geoLng) + "&hourly=temperature_2m,weathercode&models=best_match&temperature_unit=fahrenheit";
+  //Make the jQuery.getJSON request
+  $.getJSON( URLRequest )
+    //Success promise
+    .done(function( data ) {
+      var wTemperature = data.current_weather.temperature;
+      alert('worked! temperature right now in ' + data.timezone + ' is ' + wTemperature);
+      // lots of results available on the data object
+      // use the results to populate the GUI here
+    })
+    //Error promise
+    .fail(function() {
+      alert('failed to gather weather information, sorry for the inconvenience');
+    }
+  );
 }
